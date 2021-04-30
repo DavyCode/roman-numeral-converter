7b839248b4f13bb7e70d4b4c3cd8f5ec0db0e414
Paul Azemoh <pdaswift3009@gmail.com>
2021-04-30 03:11:29 +0100

    fix: added info message, display error component

--- components/converter/Converter.tsx
+++ components/converter/Converter.tsx
@@ -1,24 +1,36 @@
-import React, { Fragment }  from 'react';
+import React, { Fragment, useState, useRef }  from 'react';
 import classes from './converter.module.css';
 
 function Converter() {
+  const [errorNumMsg, setErrorNumMsg] = useState(null);
+  const [errorRomMsg, setErrorRomMsg] = useState(null);
+
   return (
     <Fragment>
       <section className={classes.wrapper}>
         <h1 className={classes.title}>Convert Numbers to Roman Numerals</h1>
-        <p className={classes.subtitle}>Quickly convert from base 10 Arabic to Roman numerals.</p>
-        <p className={classes.error}>Please enter an integer greater than 0 and less than 4000.</p>
+        <p className={classes.subtitle}>Convert from base 10 Arabic to Roman numerals.</p>
+        {errorNumMsg ? (
+            <p className={classes.error}>Error: {errorNumMsg}</p>
+          ) : (
+            <p className={classes.info}>Enter a number greater than 0 (maximum: 4999)</p>
+          )
+        }        
 
-        <input className="text-input" type="text" placeholder="2020" />
-        <p className={classes.result}>= MMXX</p>
+        <input className="text-input" type="text" placeholder="1990" />
+        <p className={classes.result}>= MCMXC</p>
       </section>
       <section className={classes.wrapper}>
         <h1 className={classes.title}>Convert Roman Numerals to Numbers</h1>
-        <p className={classes.subtitle}>Quickly convert from base 10 Arabic to Roman numerals.</p>
-        <p className={classes.error}>Please enter an integer greater than 0 and less than 4000.</p>
-
-        <input className="text-input" type="text" placeholder="2020" />
-        <p className={classes.result}>= MMXX</p>
+        <p className={classes.subtitle}>Convert from Roman Numerals base 10 Arabic.</p>
+        {errorRomMsg ? (
+            <p className={classes.error}>Error: {errorRomMsg}</p>
+          ) : (
+            <p className={classes.info}>Enter a Roman numeral (e.g. XIV)</p>
+          )
+        } 
+        <input className="text-input" type="text" placeholder="MMXX" />
+        <p className={classes.result}>= 2020</p>
       </section>
     </Fragment>
   );
